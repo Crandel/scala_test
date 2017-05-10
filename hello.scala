cb1e6d9e8e7b4b2041d9064ef435f8b2f64cb41d
Crandel <cradlemann@gmail.com>
2017-05-10 11:36:32 +0300

    Maps

--- hello.scala
+++ hello.scala
@@ -1,21 +1,16 @@
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, Map => MMap}
 object HelloWorld {
   def main(args: Array[String]) {
-    var mutList = ArrayBuffer[Int]()
-    mutList.insert(0, 2)
-    println(mutList)
-    mutList += 4
-    println(mutList)
-    mutList ++= Array(455, 677)
-    println(mutList)
-    mutList.insert(1, 5555)
-    println(mutList)
-    mutList(3) = 88888888
-    println(mutList)
-    println(f"Sum ${mutList.sum}")
-    println(f"Min ${mutList.min}")
-    println(f"Max ${mutList.max}")
-    val sortList = mutList.sortWith(_<_)
-    println(f"sortList ${sortList}")
+    val empl = Map("Manager" -> "Bob", "Secretary" -> "Jack")
+
+    if (empl.contains("Manager"))
+      println(s"Manager: ${empl("Manager")}")
+
+    val custs = MMap(1 -> "Paul", 3 -> "Tom", 5 -> "Elena")
+    println(s"custs -> ${custs.toString}")
+    custs(1) = "Margaret"
+    custs(44) = "Anton"
+    for ((k, v) <- custs)
+      println(s"$k -> $v")
   }
 }
