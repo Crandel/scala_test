b078d09bce39da6578474633dbf5afcd294ee4ad
Crandel <cradlemann@gmail.com>
2017-06-16 08:40:17 +0300

    Start convert dates

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ version := "0.01"
 
 scalaVersion := "2.11.11"
 
-// libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
+// libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.16.0"
--- src/main/scala/simple/csv.scala
+++ src/main/scala/simple/csv.scala
@@ -1,23 +1,31 @@
 package simple
 
 import java.io.PrintWriter
+import java.text.SimpleDateFormat
 import scala.collection.mutable.ArrayBuffer
-
 import scala.io.Source
 
 object CSVCleaner {
   def apply() {
     val filename = "test.csv"
-    val content = ArrayBuffer.empty[Array[String]]
+    val content = ArrayBuffer.empty[String]
     var row_number = 0
     val num = """\d+""".r
     val text = Source.fromURL(getClass.getResource("/" + filename)).getLines
     text.foreach(x => {
                    if (!x.startsWith("!")){
-                     var row = x.split("|")
-                     
-                     content += row
+                     var row = x.split('|')
+                     val outputFormat = new SimpleDateFormat("yyyy-MM-dd")
+                     val inputFormat1 = new SimpleDateFormat("dd-MMM-yyyy")
+                     val inputFormat2 = new SimpleDateFormat("MM/dd/yyyy")
+                     val first = inputFormat1.parse(row(2))
+                     println(first)
+                     row(2) = outputFormat.format(first)
+                     println("*" * 15)
+                     println(row(2))
+                     content += row.mkString("|")
                    } else if (!text.hasNext) {
+                     // get Int from String
                      row_number = num.findAllIn(x).mkString.toInt
                    }
                  })
