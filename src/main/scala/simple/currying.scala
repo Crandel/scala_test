b64cdbbb443ae27db973c30d2a4a6ec6c9a15d91
Crandel <cradlemann@gmail.com>
2019-07-08 15:52:09 +0200

    Finish currying

--- src/main/scala/simple/currying.scala
+++ src/main/scala/simple/currying.scala
@@ -10,8 +10,14 @@ object Currying {
     val partTwo = addPart(5)(7)
     println(s"addPart(5)_ with '5' arg is ${partOne(5)}")
     println(s"addPart(5)(7) with who def args is ${partTwo}")
+    println(s"Average of (90, 100, 110)(30, 40, 70)(10, 40, 85) is ${average(90, 100, 110)(30, 40, 70)(10, 40, 85)}")
   }
 
   def add(x: Int): Int => Int = y => x + y
+
   def addPart(x: Int)(y: Int): Int = x + y
+
+  def average(a: Int*)(b: Int*)(c: Int*) = {
+    0.4 * a.sum/a.length + 0.3 * b.sum/b.length + 0.3 * c.sum/c.length
+  }
 }
