586f3ee93d66a0b0f437d855fa45d8829585d42a
Crandel <cradlemann@gmail.com>
2019-07-10 07:21:46 +0200

    TestList

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,14 +1,7 @@
-import functional.{Currying, Functions}
-import primitives.ListExample
+import primitives.{Cons, TestList}
 
 object StartApp {
   def main(args: Array[String]): Unit = {
-    println("Currying example" * 3)
-    Currying()
-    println()
-    println("Funcs example" * 3)
-    Functions()
-    println("Lists")
-    ListExample()
+    TestList.test()
   }
 }
--- src/main/scala/primitives/lists.scala
+++ src/main/scala/primitives/lists.scala
@@ -1,15 +1,34 @@
 package primitives
 
-object ListExample {
-  def apply() = {
-    println("Test pattern matching in List")
-    val x = List(1,2,3,4,5) match {
-      case x :: 2 :: 4 :: _ => x
+sealed trait TestList[+A]
+case object Nil extends TestList[Nothing]
+case class Cons[+A] (head: A, tail:TestList[A]) extends TestList[A]
+
+object TestList {
+  def sum(ints: TestList[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: TestList[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): TestList[A] = {
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+  }
+
+  def test() = {
+    val x = TestList(1,2,3,4,5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
       case Nil => 42
-      case x :: y :: 3 :: 4 :: _ => x + y
-      case h :: t => h + t.sum
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + sum(t)
       case _ => 101
     }
-    println(x)
+    println(s"Result of pattern match is ${x}")
   }
 }
