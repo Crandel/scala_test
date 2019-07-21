93973f8c741fc97a20ba275a16ab7dbffc18d8e3
Crandel <cradlemann@gmail.com>
2019-07-21 13:28:46 +0200

    Add new functions + tests

--- src/main/scala/structures/trees.scala
+++ src/main/scala/structures/trees.scala
@@ -5,8 +5,27 @@ case class Leaf[A](value: A) extends FuncTree[A]
 case class Branch[A](left: FuncTree[A], right: FuncTree[A]) extends FuncTree[A]
 
 object FuncTree {
-  def sizeT(at: FuncTree[_]): Int = at match {
-    case Leaf(_) => 1
-    case Branch(l, r) => sizeT(l) + sizeT(r) + 1
-  }
+  def sizeT(at: FuncTree[_]): Int =
+    at match {
+      case Leaf(_) => 1
+      case Branch(l, r) => sizeT(l) + sizeT(r) + 1
+    }
+
+  def maximumT(at: FuncTree[Int]): Int =
+    at match {
+      case Leaf(n) => n
+      case Branch(l, r) => maximumT(l) max maximumT(r)
+    }
+
+  def depthT[A](at: FuncTree[A]): Int =
+    at match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + depthT(l) max depthT(r)
+    }
+
+  def mapT[A, B](at: FuncTree[A])(f: (A) => B): FuncTree[B] =
+    at match {
+      case Leaf(n) => Leaf(f(n))
+      case Branch(l, r) => Branch(mapT(l)(f), mapT(r)(f))
+    }
 }
--- src/test/scala/structures/FuncTreeTest.scala
+++ src/test/scala/structures/FuncTreeTest.scala
@@ -4,15 +4,37 @@ import org.scalatest._
 import structures.FuncTree._
 
 class FuncTreeTest extends FunSuite {
-  val test_int_tree = Branch(Branch(Leaf(3), Leaf(5)), Branch(Leaf(4), Leaf(6)))
-  val test_str_tree = Branch(Branch(Leaf("abc"), Leaf("defg")), Branch(Leaf("hijklm"), Leaf("nopqrstuvw")))
-  test("sizeT function") {
-    assertResult(7) {
+  val test_int_tree: FuncTree[Int] = Branch(Branch(Leaf(3), Leaf(5)), Branch(Leaf(4), Leaf(6)))
+  val test_str_tree: FuncTree[String] = Branch(Branch(Leaf("abc"), Leaf("defg")), Leaf("ffffff"))
+  test("sizeT function"){
+    assertResult(7){
       sizeT(test_int_tree)
     }
 
-    assertResult(7) {
+    assertResult(5){
       sizeT(test_str_tree)
+    }
+  }
+
+  test("maximumT function"){
+    assertResult(6){
+      maximumT(test_int_tree)
+    }
+  }
+
+  test("depthT function"){
+    assertResult(3){
+      depthT(test_int_tree)
+    }
+
+    assertResult(3){
+      depthT(test_str_tree)
+    }
+  }
+
+  test("mapT function"){
+    assertResult(Branch(Branch(Leaf("abc111"), Leaf("defg111")), Leaf("ffffff111"))){
+      mapT(test_str_tree)(x => x + "111")
     }
   }
 }
